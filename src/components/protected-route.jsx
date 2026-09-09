b51c3bb9745713e16228d89c0a8e504dd1b70c2b
AnkitGanvir <ankitganvir2004@gmail.com>
2026-09-10 00:23:09 +0530

    Fix Clerk auth loop in ProtectedRoute

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,20 +1,26 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
 
 const ProtectedRoute = ({ children }) => {
   const { isSignedIn, isLoaded, user } = useUser();
   const { pathname } = useLocation();
 
-  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  if (isLoaded && !isSignedIn) {
     return <Navigate to="/?sign-in=true" />;
   }
 
   if (
-    user !== undefined &&
+    user &&
     !user?.unsafeMetadata?.role &&
     pathname !== "/onboarding"
-  )
+  ) {
     return <Navigate to="/onboarding" />;
+  }
 
   return children;
 };
